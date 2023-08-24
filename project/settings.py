f81f64379a7d6d89268febb6feb218d613748325
zbari023 <barry450643@gmail.com>
2023-08-24 18:08:17 +0200

    add context_proccessor to the navbar

--- project/settings.py
+++ project/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'settings.company_context_proccesor.get_company_data',
             ],
         },
     },
