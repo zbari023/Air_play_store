cd58626528c5e81e4f3917dbd548a233ac2e3dc3
zbari023 <barry450643@gmail.com>
2023-08-24 17:28:11 +0200

    add Company model in admin

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=40)
+    email_us = models.CharField(max_length=25, null=True , blank=True)
+    subtitle = models.TextField(max_length=200, null=True , blank=True)
+    phones = models.TextField(max_length=200, null=True , blank=True)
+    address = models.TextField(max_length=200, null=True , blank=True)
+    fb_link = models.URLField(null=True , blank=True)
+    insta_link = models.URLField(null=True , blank=True)
+    x_link = models.URLField(null=True , blank=True)
+    youtube_link = models.URLField(null=True , blank=True)
+
+    
+    def __str__(self):
+        return self.name
+    
