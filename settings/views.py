918fece6f2b0cb0f8eb5283c23229209cc05b890
zbari023 <barry450643@gmail.com>
2023-08-24 19:53:45 +0200

    costumize home page

--- settings/views.py
+++ settings/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from products.models import Products
 # Create your views here.
 
 def home(request):
+    products = Products.objects.all()[:12] # just for limit products
     
-    
-    return render(request,'settings/home.html',{})
+    return render(request,'settings/home.html',{'products':products})
