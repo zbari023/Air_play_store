f11a4aa6c9b253a9778a5707c063c30a2b3878f3
zbari023 <barry450643@gmail.com>
2023-08-29 19:05:07 +0200

    add signup signals

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    list_filter = ['user']
+    search_fields = ['user']
+admin.site.register(UserProfile, ProductAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -5,8 +5,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='accounts', blank=True, null=True)
+    address = models.CharField(max_length=200)
     # Add other fields you want in the user profile
 
     def __str__(self):
-        return self.user.username
+        return str(self.user)
 
