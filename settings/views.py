249ce95b89a63ad7b7912705ab570b8fc85082ac
zbari023 <barry450643@gmail.com>
2023-09-09 18:21:51 +0200

    fix and adding the data of current user in his profile

--- settings/views.py
+++ settings/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from products.models import Products
+from accounts.models import UserProfile
 # Create your views here.
 
 def home(request):
@@ -16,5 +18,23 @@ def why(request):
 def testimonial(request):
     return render(request,'settings/testimonial.html',{})
 
+
+@login_required
 def profiluser(request):
-    return render(request,'settings/profil_user.html',{})
+    user_profile = request.user.user_profile
+
+    # Retrieve the profile fields
+    bio = user_profile.bio
+    profile_picture = user_profile.profile_picture
+    address = user_profile.address
+    code = user_profile.code
+
+    # Add other fields you want to retrieve
+
+    return render(request, 'settings/profil_user.html', {
+        'bio': bio,
+        'profile_picture': profile_picture,
+        'address': address,
+        'code': code,
+        # Add other fields you want to pass to the template
+    })
